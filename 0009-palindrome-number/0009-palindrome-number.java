b7a57f557c858941027c113e7f3e145657f85724
Vishal Kumar Sinha <vishalsinha.inbox@gmail.com>
2026-08-18 10:37:00 +0530

    Time: 5 ms (81.45%), Space: 45.9 MB (53.48%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,36 +1,37 @@
 class Solution {
     public boolean isPalindrome(int x) {  
         
-    //     if (x < 0) {
-    //         return false;
-    //     }
-
-    //     int reversed = 0;
-    //     int original = x;
-
-    //     while (x != 0) {
-    //         int lastDigit = x % 10;
-    //         reversed = (reversed * 10) + lastDigit;
-    //         x /= 10; 
-    //     }
-
-    //     if(original==reversed)
-    //         return true;
-
-
-    // return false;
-
-
-    String s = String.valueOf(x); // Convert to String
-        int n = s.length(); // Store the String length to int n
-
-        for (int i=0; i<n/2; i++) {
-            // beginning and from ending are same, if not we return false
-            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        if (x < 0) {
+            return false;
         }
 
-        return true;
+        int reversed = 0;
+        int original = x;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+            reversed = (reversed * 10) + lastDigit;
+            x /= 10; 
+        }
+
+        if(original==reversed)
+            return true;
+
+
+    return false;
+
     }
+
+    // String s = String.valueOf(x); // Convert to String
+    //     int n = s.length(); // Store the String length to int n
+
+    //     for (int i=0; i<n/2; i++) {
+    //         // beginning and from ending are same, if not we return false
+    //         if (s.charAt(i) != s.charAt(n-i-1)) return false;
+    //     }
+
+    //     return true;
+    // }
         
     
 }
