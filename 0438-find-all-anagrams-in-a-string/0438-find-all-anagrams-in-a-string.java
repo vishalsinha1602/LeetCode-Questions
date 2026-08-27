c76f6d6b56ba7778504b735bdde75b01e202c216
Vishal Kumar Sinha <vishalsinha.inbox@gmail.com>
2026-08-27 23:23:56 +0530

    Time: 357 ms (11.6%), Space: 47.2 MB (27.4%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+
+        List<Integer> res = new ArrayList<>();
+
+        int n = s.length();
+        int k = p.length();
+
+       
+        int[] pCount = new int[26];
+
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+       
+        for (int i = 0; i <= n - k; i++) {
+
+         
+            int[] sCount = new int[26];
+
+            for (int j = i; j <= i + k - 1; j++) {
+                sCount[s.charAt(j) - 'a']++;
+            }
+
+            // Agar dono ka frequency same hai
+            if (Arrays.equals(pCount, sCount)) {
+                res.add(i);
+            }
+        }
+
+        return res;
+    }
+}
