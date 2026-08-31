1bafac577b885e631040d8fdb3ede9bd7e1c564f
Vishal Kumar Sinha <vishalsinha.inbox@gmail.com>
2026-08-31 15:08:53 +0530

    Time: 1 ms (99.94%), Space: 80.6 MB (8.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
 
+        // kadane algorithm
+        // choice 1: ya toh phle wale ke sath continue kro
+        // choice 2: ya toh abhi wale ke sath start kro
+
         int currentSum = nums[0];
         int maxSum = currentSum;
 
