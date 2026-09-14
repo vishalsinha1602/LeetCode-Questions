6e759d255e9c759e5f7bf72c4853520dd1a7c751
Vishal Kumar Sinha <vishalsinha.inbox@gmail.com>
2026-09-14 22:26:28 +0530

    Time: 1 ms (77.42%), Space: 42.6 MB (21.37%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            int x = 0;
+            while (n != 0) {
+                x += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = x;
+        }
+        return n == 1;
+    }
+}
