61ee7882c24948230e9a7d87b48cff56f56b08f0
Vishal Kumar Sinha <vishalsinha.inbox@gmail.com>
2026-09-16 19:34:37 +0530

    Time: 23 ms (92.96%), Space: 49 MB (34.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -7,20 +7,20 @@ class Solution {
 
         map.put(0, 1);
 
-        int count = 0;
-        int prefixSum = 0;
+        int count=0;
+        int prefixSum=0;
 
-        for (int n : nums) {
+        for(int num:nums)
+        {
+            prefixSum+=num;
 
-            prefixSum += n;
-
-            if (map.containsKey(prefixSum - k)) {
-                count += map.get(prefixSum - k);
+            if(map.containsKey(prefixSum-k))
+            {
+                count = count + map.get(prefixSum-k);
             }
 
-            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            map.put(prefixSum,map.getOrDefault(prefixSum,0)+1);
         }
-
         return count;
     }
 }
